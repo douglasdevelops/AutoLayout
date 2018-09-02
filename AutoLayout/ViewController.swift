0f0a6e82d1b7e47f4a6b17461d00f295dd9e1854
Douglas Spencer <douglasdevelops@gmail.com>
2018-09-02 14:11:56 -0400

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Douglas Spencer on 9/2/18.
+//  Copyright © 2018 Douglas Spencer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
